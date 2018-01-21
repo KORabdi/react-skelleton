7c3dc8db0786e0f59306442e5b77626fb2772f22
kozokkon <kostantin.k@seznam.cz>
2018-01-21 18:16:31 +0100

    Adding logger

--- src/store.js
+++ src/store.js
@@ -3,33 +3,36 @@ import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './modules'
+import { createLogger } from 'redux-logger'
+export const history = createHistory();
 
-export const history = createHistory()
+const loggerMiddleware = createLogger();
 
-const initialState = {}
-const enhancers = []
+const initialState = {};
+const enhancers = [];
 const middleware = [
     thunk,
-    routerMiddleware(history)
-]
+    routerMiddleware(history),
+    loggerMiddleware
+];
 
 if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.devToolsExtension
+    const devToolsExtension = window.devToolsExtension;
 
     if (typeof devToolsExtension === 'function') {
-        enhancers.push(devToolsExtension())
+        enhancers.push(devToolsExtension());
     }
 }
 
 const composedEnhancers = compose(
     applyMiddleware(...middleware),
     ...enhancers
-)
+);
 
 const store = createStore(
     rootReducer,
     initialState,
     composedEnhancers
-)
+);
 
 export default store
